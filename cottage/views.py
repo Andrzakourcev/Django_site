dc800cec78bd272d58876cf8bce58d611cc31272
Andrey Zakourcev <andrzakourcev@mail.ru>
2025-02-19 20:26:22 +0300

    Start project

---
+++ cottage/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(requests): #переменная requests - ссылка на класс HttpRequest, которая содежит информацию о запросе
+    return HttpResponse("Страница приложения cottage")
+
+
